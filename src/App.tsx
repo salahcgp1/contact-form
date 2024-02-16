1857d9805aa82d0bc34e9351f68c113f7f6a1832
Codux <codux@codux.com>
2024-02-16 21:03:48 +0400

    Showing Form submit data to console log

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,40 @@ import { FormRow } from './components/form-row/form-row';
 import { FormLine } from './components/form-line/form-line';
 
 function App() {
+
+    function handOnSubmit (e: React.SyntheticEvent<HTMLFormElement>) {
+      e.preventDefault();
+
+      const formData: Record<string, string> = {};
+
+        function isInputNamedElement(e: Element): e is HTMLInputElement & { name: string } {
+            return 'value' in e && 'name' in e;
+        }
+
+        Array.from(e.currentTarget.elements).filter(isInputNamedElement).forEach((field) => {
+            if (!field.name) return;
+            formData[field.name] = field.value;
+        });
+        
+      console.log('formData', formData);
+
+    // you can activate his code only when
+    //   fetch('/.netlify/functions/mail', {
+    //     method: 'POST',
+    //     body: JSON.stringify(formData)
+    // }).then(response => {
+    //     // Do something here on success!
+    // }).catch(error => {
+    //     // Do something here on error!
+    // });
+
+    }
+
     const [count, setCount] = useState(0);
 
     return (
         <div className={styles.App}>
-            <form>
+            <form onSubmit={handOnSubmit}>
                 <FormLine>
                     <Label htmlFor="Name">Name </Label>
                     <Input id="Name" name="Name" />
